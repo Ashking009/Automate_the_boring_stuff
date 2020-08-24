edf6866a2bbf47c863b33f49c1794dcf0b70f4e6
Ashish Mishra <50423720+Ashking009@users.noreply.github.com>
2020-08-24 20:01:42 +0530

    Update Auto_join_google_meet.py

--- Auto_join_google_meet.py
+++ Auto_join_google_meet.py
@@ -3,6 +3,7 @@
 # Only dependency needed is selenium (pip install selenium) and 
 # chromedriver(As I used Chrome browser for this download it from https://chromedriver.chromium.org/ 
 # extract the zip from above link and paste the path at line 14 executable_path = " path " like  C:\Drivers\chromedriver.exe
+# put meeting link in line 17 , your gmail in line 23 and password in line 25
 # Thats it you are ready to rock
 
 from selenium import webdriver
@@ -14,10 +15,10 @@ import time
 driver = webdriver.Chrome(executable_path="Chrome_driver_path")
 driver.implicitly_wait(5)
 meet_link ="Meeting link"
+
+# Authenticating via stackoverflow
 driver.get("https://stackoverflow.com/users/login")
-
 driver.find_element_by_xpath('//*[@id="openid-buttons"]/button[1]').click()
-
 wait=WebDriverWait(driver, 10)
 wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id=\"identifierId\"]"))).send_keys("Your Gmail")
 driver.find_element_by_xpath('//*[@id="identifierNext"]/div/button/div[2]').click()
@@ -25,6 +26,7 @@ wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id=\"password\"]/div[1]/d
 driver.find_element_by_xpath("//*[@id=\"passwordNext\"]/div/button/div[2]").click()
 time.sleep(10) # important it takes time to load Stackoverflow so adjust according to your internet speed 
 
+# Opening Meeting after logging in
 driver.get(meet_link)
 wait.until(EC.element_to_be_clickable((By.XPATH,"//*[@id=\"yDmH0d\"]/div[3]/div/div[2]/div[3]/div/span/span"))).click()
 wait.until(EC.element_to_be_clickable((By.XPATH,"//*[@id=\"yDmH0d\"]/c-wiz/div/div/div[4]/div[3]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/span/span"))).click()
